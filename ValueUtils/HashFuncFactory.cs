644d9e270e60d49771ec5ff486a3086d042214fb
Eamon Nerbonne <eamon@nerbonne.org>
2014-05-26 13:13:07 +0200

    Initial Project files

---
+++ ValueUtils/HashFuncFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueUtils {
+	static class HashFuncFactory<T> {
+		public static readonly Func<T, int> Instance = Create();
+
+		static Func<T, int> Create() {
+			var fields = typeof(T).GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+			var paramExpr = Expression.Parameter(typeof(T), "valueToHash");
+
+			throw new NotImplementedException();
+		}
+
+	}
+}
