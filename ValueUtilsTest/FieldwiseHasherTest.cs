734424aa0b2fb081e2ec53a3c6c0675e860505c2
Eamon Nerbonne <eamon@nerbonne.org>
2014-05-27 09:08:10 +0200

    Add notes for why this struct was constructed as is

--- ValueUtilsTest/FieldwiseHasherTest.cs
+++ ValueUtilsTest/FieldwiseHasherTest.cs
@@ -10,10 +10,10 @@ using Xunit;
 namespace ValueUtilsTest {
     public class FieldwiseHasherTest {
         struct SampleStruct {
-            public int value;
-            public short shortvalue;
-            public readonly string hmm;
-            byte last;
+            int value;
+            public short shortvalue; //we want to check various underlying value types
+            public readonly string hmm; //and at least one reference type member
+            byte last; //also different combos of readonly/private/protected
             public SampleStruct(int a, short b, string c, byte d) {
                 value = a;
                 shortvalue = b;
