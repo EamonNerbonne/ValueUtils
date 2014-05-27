d7d1a63d272db0cb81d0128603e48b5ad1e42123
Eamon Nerbonne <eamon@nerbonne.org>
2014-05-27 08:55:16 +0200

    Implement a few basic sanity tests of the FieldwiseHash
    --HG--
    rename : ValueUtilsTest/HashFuncForTest.cs => ValueUtilsTest/FieldwiseHasherTest.cs

---
+++ ValueUtilsTest/FieldwiseHasherTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpressionToCodeLib;
+using ValueUtils;
+using Xunit;
+
+namespace ValueUtilsTest {
+    class FieldwiseHasherTest {
+        struct SampleStruct {
+            public int value;
+            public short shortvalue;
+            public readonly string hmm;
+            byte last;
+            public SampleStruct(int a, short b, string c, byte d) {
+                value = a;
+                shortvalue = b;
+                hmm = c;
+                last = d;
+            }
+        }
+        static readonly Func<SampleStruct, int> hash = FieldwiseHasher<SampleStruct>.Instance;
+
+        [Fact]
+        public void IdenticalStructsWork() {
+            PAssert.That(() =>
+                hash(new SampleStruct(1, 2, "3", 4))
+                == hash(new SampleStruct(1, 2, "3", 4)));
+        }
+
+        [Fact]
+        public void ChangingMembersHaveDifferentHashCodes() {
+            PAssert.That(() =>
+                hash(new SampleStruct(1, 2, "3", 4))
+                != hash(new SampleStruct(11, 2, "3", 4)));
+            PAssert.That(() =>
+                hash(new SampleStruct(1, 2, "3", 4))
+                != hash(new SampleStruct(1, 12, "3", 4)));
+            PAssert.That(() =>
+                hash(new SampleStruct(1, 2, "3", 4))
+                != hash(new SampleStruct(1, 2, "13", 4)));
+            PAssert.That(() =>
+                hash(new SampleStruct(1, 2, "3", 4))
+                != hash(new SampleStruct(1, 2, "3", 14)));
+        }
+
+        [Fact]
+        public void HashFuncWorksForExternalClasses() {
+
+            PAssert.That(() =>
+                hash(new SampleStruct(1, 2, "3", 4))
+                !=hash( new SampleStruct(11, 2, "3", 4)));
+            PAssert.That(() =>
+                hash(new SampleStruct(1, 2, "3", 4))
+                != hash(new SampleStruct(1, 12, "3", 4)));
+            PAssert.That(() =>
+                hash(new SampleStruct(1, 2, "3", 4))
+                != hash(new SampleStruct(1, 2, "13", 4)));
+            PAssert.That(() =>
+                hash(new SampleStruct(1, 2, "3", 4))
+                != hash(new SampleStruct(1, 2, "3", 14)));
+        }
+
+    }
+
+    
+}
