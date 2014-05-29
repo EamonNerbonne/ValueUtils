553169098369f48158835c55168863041f898dec
Eamon Nerbonne <eamon@nerbonne.org>
2014-05-29 22:10:51 +0200

    Add tricky cases to test class:  - nullable primitives  - non-nullable custom structs  - nullable structs

--- ValueUtilsTest/SampleClass.cs
+++ ValueUtilsTest/SampleClass.cs
@@ -5,9 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ValueUtilsTest {
+    struct CustomStruct {
+        public int Bla;
+    }
+
     class SampleClass {
         public SampleEnum AnEnum;
-        
+        public int? NullableField;
+        public CustomStruct PlainStruct;
+        public CustomStruct? NullableStruct;
+
         public string AutoPropWithPrivateBackingField { get; set; }
     }
 
