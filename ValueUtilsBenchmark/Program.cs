ed4a8241ef0457b9929eba6205271e070ed59d29
Eamon Nerbonne <eamon@nerbonne.org>
2014-05-30 13:27:59 +0200

    Extend benchmark program to generate a bunch of html table results.

--- ValueUtilsBenchmark/Program.cs
+++ ValueUtilsBenchmark/Program.cs
@@ -5,30 +5,98 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using ExpressionToCodeLib;
 using MoreLinq;
 
 namespace ValueUtilsBenchmark {
+    struct CollisionStats {
+        public int DistinctHashCodes, DistinctValues;
+        public double Rate { get { return (DistinctValues - DistinctHashCodes - 1) / (DistinctValues - 1); } }
+    }
+    class HashAnalysisResult {
+        public string Name;
+        public CollisionStats Collisions;
+        public double GetHashCodeMS, EqualsMS, DistinctCountMS, DictionaryMS;
+        public XElement ToTableRow() {
+            return new XElement("tr",
+                new XElement("td", Name),
+                new XElement("td", (Collisions.Rate * 100).ToString("f2") + "%"),
+                new XElement("td", Collisions.DistinctHashCodes + " / " + Collisions.DistinctValues),
+                new XElement("td", DictionaryMS.ToString("f3")),
+                new XElement("td", DistinctCountMS.ToString("f3")),
+                new XElement("td", EqualsMS.ToString("f3")),
+                new XElement("td", GetHashCodeMS.ToString("f3"))
+                );
+        }
+        public static XElement ToTableHead(string title) {
+            return
+                new XElement("thead",
+                    new XElement("tr",
+                        new XElement("th",
+                             new XAttribute("colspan", 7),
+                             title
+                        )
+                    ),
+                    new XElement("tr",
+                        new XElement("th", "Name"),
+                        new XElement("th", "Collisions"),
+                        new XElement("th", "Distinct Hashcodes"),
+                        new XElement("th", ".ToDictionary(...) (ms)"),
+                        new XElement("th", ".Distinct().Count() (ms)"),
+                        new XElement("th", ".Equals(...) (ms)"),
+                        new XElement("th", ".GetHashCode(...) (ms)")
+                    )
+                );
+        }
+        public static XElement ToTable(string title, IEnumerable<HashAnalysisResult> results) {
+            return
+                new XElement("table",
+                    ToTableHead(title),
+                    new XElement("tbody",
+                        results.Select(r => r.ToTableRow())
+                    )
+                );
+        }
+    }
+
     class Program {
         static void Main(string[] args) {
-            BenchmarkComplicatedCases();
-            Console.WriteLine();
-            BenchmarkNastyNestedCases();
-            Console.WriteLine();
-            BenchmarkDuplicatePairCases();
-            Console.WriteLine();
-            BenchmarkSymmetricalCases();
-            Console.WriteLine();
-            BenchmarkIntPairCases();
+
+            var complicatedTable = HashAnalysisResult.ToTable(
+                "Complicated Case with enums, nullables, and strings", 
+                BenchmarkComplicatedCases()
+                );
+            var intpairTable = HashAnalysisResult.ToTable(
+                "A simple pair of ints",
+                BenchmarkIntPairCases()
+                );
+            var duplicationTable = HashAnalysisResult.ToTable(
+                "int-pair with both ints having the same value",
+                BenchmarkDuplicatePairCases()
+                );
+            var symmetricalTable = HashAnalysisResult.ToTable(
+                "int-pair with a dataset in which both the object and it's mirror image are present",
+                BenchmarkSymmetricalCases()
+                );
+            var nestedTable = HashAnalysisResult.ToTable(
+                "int-pair with self-reference; data set contains only one level of nesting with nested values being symmetrical to their containers",
+                BenchmarkNastyNestedCases()
+                );
+
+            var tables = 
+                new XElement("div",
+                    complicatedTable, intpairTable, duplicationTable, symmetricalTable, nestedTable);
+            Console.WriteLine(tables.ToString());
         }
 
-        static void BenchmarkNastyNestedCases() {
+        static IEnumerable<HashAnalysisResult> BenchmarkNastyNestedCases() {
             Console.WriteLine("NastyNested:");
             var manuals = (
                 from a in MoreEnumerable.Generate(-10000, a => a + 6).TakeWhile(a => a < 14100)
-                from b0 in MoreEnumerable.Generate(int.MaxValue, c => (int)(c *0.95)).TakeWhile(c => c > 0)
-                from sign in new[]{-1, 1}
-                let b = sign*b0
+                from b0 in MoreEnumerable.Generate(int.MaxValue, c => (int)(c * 0.95)).TakeWhile(c => c > 0)
+                from sign in new[] { -1, 1 }
+                let b = sign * b0
                 select new NastyNestedManual {
                     Nested = new NastyNestedManual {
                         A = b,
@@ -42,13 +110,13 @@ namespace ValueUtilsBenchmark {
             var valueObjects = manuals.Select(m => m.ToValueObject()).ToArray();
             var tuples = manuals.Select(m => m.ToTuple()).ToArray();
 
-            ProcessList(manuals);
-            ProcessList(valueObjects);
-            ProcessList(tuples);
+            yield return ProcessList(manuals);
+            yield return ProcessList(valueObjects);
+            yield return ProcessList(tuples);
         }
 
 
-        static void BenchmarkIntPairCases() {
+        static IEnumerable<HashAnalysisResult> BenchmarkIntPairCases() {
             Console.WriteLine("IntPair:");
             var manuals = (
                 from a in MoreEnumerable.Generate(-8000, a => a + 4).TakeWhile(a => a < 8000)
@@ -65,14 +133,14 @@ namespace ValueUtilsBenchmark {
             var tuples = manuals.Select(m => m.ToTuple()).ToArray();
             var structs = manuals.Select(m => m.ToStruct()).ToArray();
 
-            ProcessList(manuals);
-            ProcessList(valueObjects);
-            ProcessList(tuples);
-            ProcessList(structs);
+            yield return ProcessList(manuals);
+            yield return ProcessList(valueObjects);
+            yield return ProcessList(tuples);
+            yield return ProcessList(structs);
         }
 
 
-        static void BenchmarkSymmetricalCases() {
+        static IEnumerable<HashAnalysisResult> BenchmarkSymmetricalCases() {
             Console.WriteLine("IntPair with Symmetry:");
             var manuals = (
                 from a in MoreEnumerable.Generate(-5100, a => a + 5).TakeWhile(a => a < 5100)
@@ -90,14 +158,13 @@ namespace ValueUtilsBenchmark {
             var tuples = manuals.Select(m => m.ToTuple()).ToArray();
             var structs = manuals.Select(m => m.ToStruct()).ToArray();
 
-            ProcessList(manuals);
-            ProcessList(valueObjects);
-            ProcessList(tuples);
-            ProcessList(structs);
+            yield return ProcessList(manuals);
+            yield return ProcessList(valueObjects);
+            yield return ProcessList(tuples);
+            yield return ProcessList(structs);
         }
 
-        static void BenchmarkDuplicatePairCases() {
-            Console.WriteLine("IntPair with Duplication:");
+        static IEnumerable<HashAnalysisResult> BenchmarkDuplicatePairCases() {
             var manuals = (
                 from a in Enumerable.Range(0, 3000000)
                 select new IntPairManual {
@@ -110,14 +177,13 @@ namespace ValueUtilsBenchmark {
             var tuples = manuals.Select(m => m.ToTuple()).ToArray();
             var structs = manuals.Select(m => m.ToStruct()).ToArray();
 
-            ProcessList(manuals);
-            ProcessList(valueObjects);
-            ProcessList(tuples);
-            ProcessList(structs);
+            yield return ProcessList(manuals);
+            yield return ProcessList(valueObjects);
+            yield return ProcessList(tuples);
+            yield return ProcessList(structs);
         }
 
-        static void BenchmarkComplicatedCases() {
-            Console.WriteLine("Complicated:");
+        static IEnumerable<HashAnalysisResult> BenchmarkComplicatedCases() {
             var manuals = (
                 from a in MoreEnumerable.Generate(-500, a => a + 4).TakeWhile(a => a < 500)
                 from b0 in MoreEnumerable.Generate(int.MaxValue, b => (int)(b * 0.9)).TakeWhile(b => b > 0)
@@ -125,11 +191,11 @@ namespace ValueUtilsBenchmark {
                 let b = sign * b0
                 from c in MoreEnumerable.Generate(1, c => c << 1).TakeWhile(c => c > 0)
                 select new ComplicatedManual {
-                    AnEnum = (SeekOrigin)(c%3),
+                    AnEnum = (SeekOrigin)(c % 3),
                     A = a,
                     B = b,
                     C = c,
-                    NullableInt = a< b && b< c ? default(int?) : a&b&c,
+                    NullableInt = a < b && b < c ? default(int?) : a & b & c,
                     Label = Math.Min(a, c).ToString(),
                     Time = new DateTime(2014, 1, 1) + TimeSpan.FromSeconds(a + c + Math.Log(b0))
                 }
@@ -138,39 +204,42 @@ namespace ValueUtilsBenchmark {
             var tuples = manuals.Select(m => m.ToTuple()).ToArray();
             var structs = manuals.Select(m => m.ToStruct()).ToArray();
 
-            ProcessList(manuals);
-            ProcessList(valueObjects);
-            ProcessList(tuples);
-            ProcessList(structs);
+            yield return ProcessList(manuals);
+            yield return ProcessList(valueObjects);
+            yield return ProcessList(tuples);
+            yield return ProcessList(structs);
         }
 
-        static void ProcessList<T>(T[] objs) {
+        static HashAnalysisResult ProcessList<T>(T[] objs) {
             string name = ObjectToCode.GetCSharpFriendlyTypeName(typeof(T));
-            AnalyzeHashQuality(objs);
-            Benchmark(name + " GetHashCode()", () => {
-                foreach (var inst in objs)
-                    inst.GetHashCode();
-            });
-            Benchmark(name + " Equals()", () => {
-                for (int i = 0; i < objs.Length; i++) {
-                    objs[i].Equals(objs[i]);
-                    objs[i].Equals(objs[(i + 1) % objs.Length]);
-                }
-            });
-            if (CountDistinctHashes(objs) >= 500) //at only 500 hash values, we'll have lots and lots of hash collisions.
-                Benchmark(name + " Distinct().Count()", () => {
+            var collisions = AnalyzeHashCollisions(objs);
+            return new HashAnalysisResult {
+                Name = name,
+                Collisions = collisions,
+                GetHashCodeMS = Time(() => {
+                    foreach (var inst in objs)
+                        inst.GetHashCode();
+                }) / objs.Length,
+                EqualsMS = Time(() => {
+                    for (int i = 0; i < objs.Length; i++) {
+                        objs[i].Equals(objs[i]);
+                        objs[i].Equals(objs[(i + 1) % objs.Length]);
+                    }
+                }) / objs.Length / 2,
+                DistinctCountMS = collisions.Rate > 0.99 ? double.NaN : Time(() => {
                     objs.Distinct().Count();
-                });
-            else
-                Console.WriteLine(name + " Distinct().Count(): <too slow to complete>");
+                }) / objs.Length,
+                DictionaryMS = collisions.Rate > 0.99 ? double.NaN : Time(() => {
+                    int idx = 0;
+                    objs.ToDictionary(o => o, _ => idx++);
+                }) / objs.Length,
+            };
         }
-        static int CountDistinctHashes<T>(T[] objs) {
-            return objs.Select(o => o.GetHashCode()).Distinct().Count();
-        }
-        static void AnalyzeHashQuality<T>(T[] objs) {
-            int distinctHashes = CountDistinctHashes(objs);
-            double coverageRate = (distinctHashes - 1.0) / (objs.Length - 1.0);
-            Console.WriteLine(ObjectToCode.GetCSharpFriendlyTypeName(typeof(T)) + " has a hash coverate rate of " + (coverageRate * 100.0).ToString("f2") + "% (" + distinctHashes + " hashes over " + objs.Length + " distinct objects).");
+        static CollisionStats AnalyzeHashCollisions<T>(T[] objs) {
+            return new CollisionStats {
+                DistinctValues = objs.Length,
+                DistinctHashCodes = objs.Select(o => o.GetHashCode()).Distinct().Count()
+            };
         }
 
         static void Benchmark(string label, Action action) {
