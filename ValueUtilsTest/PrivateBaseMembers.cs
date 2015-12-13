669a2de7375cb9c9893183e0b5c66007185b4ff5
Eamon Nerbonne <eamon@nerbonne.org>
2015-12-13 18:05:03 +0100

    write tests for nasty inheritance corner cases

---
+++ ValueUtilsTest/PrivateBaseMembers.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpressionToCodeLib;
+using ValueUtils;
+using Xunit;
+
+namespace ValueUtilsTest
+{
+    class SampleBaseWithPrivateMembers
+    {
+        int SomeValue;
+        public SampleBaseWithPrivateMembers(int val) { SomeValue = val; }
+    }
+
+    class SampleSubWithPrivateInherited : SampleBaseWithPrivateMembers
+    {
+        public SampleSubWithPrivateInherited(int val) : base(val) { }
+    }
+
+    public class PrivateBaseMembers
+    {
+        [Fact]
+        public void IdenticalValuesAreEqual()
+        {
+            var a = new SampleSubWithPrivateInherited(1);
+            var b = new SampleSubWithPrivateInherited(1);
+            PAssert.That(() =>
+                FieldwiseEquality.AreEqual(a, b)
+                && !ReferenceEquals(a, b)
+                );
+        }
+
+        [Fact]
+        public void IdenticalValuesHaveSameHash()
+        {
+            var a = new SampleSubWithPrivateInherited(1);
+            var b = new SampleSubWithPrivateInherited(1);
+            PAssert.That(() =>
+                FieldwiseHasher.Hash(a) == FieldwiseHasher.Hash(b)
+                && !ReferenceEquals(a, b)
+                );
+        }
+
+        [Fact]
+        public void NonIdenticalValuesAreNotEqual()
+        {
+            var a = new SampleSubWithPrivateInherited(1);
+            var b = new SampleSubWithPrivateInherited(2);
+            PAssert.That(() =>
+                !FieldwiseEquality.AreEqual(a, b)
+                && !ReferenceEquals(a, b)
+                );
+        }
+
+        [Fact]
+        public void NonIdenticalValuesHaveDifferentHash()
+        {
+            var a = new SampleSubWithPrivateInherited(1);
+            var b = new SampleSubWithPrivateInherited(2);
+            PAssert.That(() =>
+                FieldwiseHasher.Hash(a) != FieldwiseHasher.Hash(b)
+                && !ReferenceEquals(a, b)
+                );
+        }
+    }
+}
+    
