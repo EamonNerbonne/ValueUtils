2b016aaa558e443687290ef87649047476b41ebb
Eamon Nerbonne <eamon@nerbonne.org>
2015-12-14 20:37:42 +0100

    Reduce line length

--- ValueUtilsTest/BaseTypesWork.cs
+++ ValueUtilsTest/BaseTypesWork.cs
@@ -14,49 +14,57 @@ namespace ValueUtilsTest
         [Fact]
         public void BaseTypesOfIntAreJustInt()
         {
-            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(int)).SequenceEqual(new[] { typeof(int) }));
+            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(int))
+                .SequenceEqual(new[] { typeof(int) }));
         }
 
         [Fact]
         public void BaseTypesOfObjectAreEmpty()
         {
-            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(object)).SequenceEqual(new Type[0]));
+            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(object))
+                .SequenceEqual(new Type[0]));
         }
 
         [Fact]
         public void BaseTypesOfAnEnumAreJustThatEnum()
         {
-            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(SampleEnum)).SequenceEqual(new[] { typeof(SampleEnum) }));
+            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(SampleEnum))
+                .SequenceEqual(new[] { typeof(SampleEnum) }));
         }
 
         [Fact]
         public void BaseTypesOfAStructAreJustThatStruct()
         {
-            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(SampleStruct)).SequenceEqual(new[] { typeof(SampleStruct) }));
+            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(SampleStruct))
+                .SequenceEqual(new[] { typeof(SampleStruct) }));
         }
 
         [Fact]
         public void BaseTypesOfAGenericStructAreJustThatStruct()
         {
-            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(KeyValuePair<int,string>)).SequenceEqual(new[] { typeof(KeyValuePair<int, string>) }));
+            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(KeyValuePair<int, string>))
+                .SequenceEqual(new[] { typeof(KeyValuePair<int, string>) }));
         }
 
         [Fact]
         public void BaseTypesOfAPlainClassAreJustThatClass()
         {
-            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(SampleClass)).SequenceEqual(new[] { typeof(SampleClass) }));
+            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(SampleClass))
+                .SequenceEqual(new[] { typeof(SampleClass) }));
         }
 
         [Fact]
         public void BaseTypesOfASubClassAreFirstSubClassThenBaseClass()
         {
-            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(SampleSubClass)).SequenceEqual(new[] { typeof(SampleSubClass), typeof(SampleClass) }));
+            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(SampleSubClass))
+                .SequenceEqual(new[] { typeof(SampleSubClass), typeof(SampleClass) }));
         }
 
         [Fact]
         public void BaseTypesOfASubSubClassIncludeTheCompleteChain()
         {
-            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(SampleSubSubClass)).SequenceEqual(new[] { typeof(SampleSubSubClass), typeof(SampleSubClass), typeof(SampleClass) }));
+            PAssert.That(() => ReflectionHelper.WalkMeaningfulInheritanceChain(typeof(SampleSubSubClass))
+                .SequenceEqual(new[] { typeof(SampleSubSubClass), typeof(SampleSubClass), typeof(SampleClass) }));
         }
     }
 }
