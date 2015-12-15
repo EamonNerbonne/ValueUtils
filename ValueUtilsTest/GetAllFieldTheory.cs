196a192f71acfb271fd6792c5738d170b1cf21f2
Eamon Nerbonne <eamon@nerbonne.org>
2015-12-15 10:31:41 +0100

    (minor) autoformat

--- ValueUtilsTest/GetAllFieldTheory.cs
+++ ValueUtilsTest/GetAllFieldTheory.cs
@@ -14,39 +14,42 @@ namespace ValueUtilsTest
         [Fact]
         public void SampleStructHasFourFields()
         {
-            PAssert.That(() => ReflectionHelper.GetAllFields(typeof(SampleStruct)).Select(f=>f.Name)
-                .SequenceEqual(new[] { "value", "shortvalue", "hmm", "last" }));
+            PAssert.That(() => ReflectionHelper.GetAllFields(typeof (SampleStruct)).Select(f => f.Name)
+                .SequenceEqual(new[] {"value", "shortvalue", "hmm", "last"}));
         }
 
         [Fact]
         public void SampleClassHasFiveFields()
         {
-            PAssert.That(() => ReflectionHelper.GetAllFields(typeof(SampleClass)).Select(f=>f.FieldType)
-                .SequenceEqual(new[] { typeof(SampleEnum), typeof(int?), typeof(CustomStruct), typeof(CustomStruct?), typeof(string) }));
+            PAssert.That(() => ReflectionHelper.GetAllFields(typeof (SampleClass)).Select(f => f.FieldType)
+                .SequenceEqual(new[]
+                {typeof (SampleEnum), typeof (int?), typeof (CustomStruct), typeof (CustomStruct?), typeof (string)}));
         }
 
         [Fact]
         public void SampleSubClassHasBaseFieldsThenOwnFields()
         {
-            PAssert.That(() => ReflectionHelper.GetAllFields(typeof(SampleSubClassWithFields)).Take(5)
-                .SequenceEqual(ReflectionHelper.GetAllFields(typeof(SampleClass))));
+            PAssert.That(() => ReflectionHelper.GetAllFields(typeof (SampleSubClassWithFields)).Take(5)
+                .SequenceEqual(ReflectionHelper.GetAllFields(typeof (SampleClass))));
 
-            PAssert.That(() => ReflectionHelper.GetAllFields(typeof(SampleSubClassWithFields)).Select(f => f.Name).Skip(5)
-                .SequenceEqual(new[] { "SubClassField" }));
+            PAssert.That(
+                () => ReflectionHelper.GetAllFields(typeof (SampleSubClassWithFields)).Select(f => f.Name).Skip(5)
+                    .SequenceEqual(new[] {"SubClassField"}));
         }
 
         [Fact]
         public void FieldsIncludeBaseProtectedFieldsExactlyOnce()
         {
-            PAssert.That(() => ReflectionHelper.GetAllFields(typeof(SampleSubWithProtectedInherited)).Select(f => f.Name)
-                .SequenceEqual(new[] { "SomeValue" }));
+            PAssert.That(
+                () => ReflectionHelper.GetAllFields(typeof (SampleSubWithProtectedInherited)).Select(f => f.Name)
+                    .SequenceEqual(new[] {"SomeValue"}));
         }
 
         [Fact]
         public void FieldsIncludeBasePrivateFieldsExactlyOnce()
         {
-            PAssert.That(() => ReflectionHelper.GetAllFields(typeof(SampleSubWithPrivateInherited)).Select(f => f.Name)
-                .SequenceEqual(new[] { "SomeValue" }));
+            PAssert.That(() => ReflectionHelper.GetAllFields(typeof (SampleSubWithPrivateInherited)).Select(f => f.Name)
+                .SequenceEqual(new[] {"SomeValue"}));
         }
     }
 }
