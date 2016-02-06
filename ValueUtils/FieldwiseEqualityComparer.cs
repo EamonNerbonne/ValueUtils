abbd34ebb60f572e773b7cc089a5d694849f5f0f
Eamon Nerbonne <eamon@nerbonne.org>
2016-02-06 15:01:24 +0100

    ...for the new, simpler approach.
    This comparer just wraps the existing functionality.

---
+++ ValueUtils/FieldwiseEqualityComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValueUtils
+{
+    /// <summary>
+    /// Implements an fieldwise IEqualityComparer singleton.
+    /// This comparer simply wraps FieldwiseHasher and FieldwiseEquality.
+    /// </summary>
+    public sealed class FieldwiseEqualityComparer<T> : IEqualityComparer<T>
+    {
+        public static readonly FieldwiseEqualityComparer<T> Instance = new FieldwiseEqualityComparer<T>();
+        public bool Equals(T x, T y) => equalsDelegate(x, y);
+        public int GetHashCode(T obj) => getHashCodeDelegate(obj);
+
+        private FieldwiseEqualityComparer() { }
+        readonly Func<T, T, bool> equalsDelegate = FieldwiseEquality<T>.Instance;
+        readonly Func<T, int> getHashCodeDelegate = FieldwiseHasher<T>.Instance;
+    }
+}
