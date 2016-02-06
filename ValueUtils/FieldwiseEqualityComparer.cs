d0fa4e96d25a52b37b7967bf6af496f03ad1353a
Eamon Nerbonne <eamon@nerbonne.org>
2016-02-06 15:03:25 +0100

    Add comment explaining downside of wrapper approach

--- ValueUtils/FieldwiseEqualityComparer.cs
+++ ValueUtils/FieldwiseEqualityComparer.cs
@@ -9,6 +9,10 @@ namespace ValueUtils
     /// </summary>
     public sealed class FieldwiseEqualityComparer<T> : IEqualityComparer<T>
     {
+        //The downside of wrapping is that a consumer effective will pay the price of two 
+        //interface calls: once to e.g. IEqualityComparer.Equals and once to Func<...>.Invoke
+        //It's not a huge issue, and a smart JIT might inline those, but I'm not holding my breath.
+
         public static readonly FieldwiseEqualityComparer<T> Instance = new FieldwiseEqualityComparer<T>();
         public bool Equals(T x, T y) => equalsDelegate(x, y);
         public int GetHashCode(T obj) => getHashCodeDelegate(obj);
