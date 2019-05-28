543a59c0e0bea5615eda39f9ffb465e60630a533
Eamon Nerbonne <eamon@nerbonne.org>
2019-05-28 11:07:43 +0200

    Code cleanup
     - reformat
     - extract method
     - add xml doc

--- ValueUtils/FieldwiseEquality.cs
+++ ValueUtils/FieldwiseEquality.cs
@@ -1,32 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
-namespace ValueUtils {
-    public static class FieldwiseEquality {
+namespace ValueUtils
+{
+    /// <summary>
+    ///     Implements field-wise equality via reflection-based code generation.
+    /// </summary>
+    public static class FieldwiseEquality
+    {
         /// <summary>
-        /// Checks whether two objects of the same type are field-wise equal.  Type resolution is done 
-        /// statically, which allows fast code (similar to hand-rolled performance).
-        /// However, warning: if the objects are of compile-time type BaseType, but at runtime turn out
-        /// to be SubClass, then only the fields of BaseType will be checked.
-        /// 
-        /// This is simply a type-inference friendly wrapper around FieldwiseEquality&lt;&gt;.Instance
+        ///     Checks whether two objects of the same type are field-wise equal.  Type resolution is done
+        ///     statically, which allows fast code (similar to hand-rolled performance).
+        ///     However, warning: if the objects are of compile-time type BaseType, but at runtime turn out
+        ///     to be SubClass, then only the fields of BaseType will be checked.
+        ///     This is simply a type-inference friendly wrapper around FieldwiseEquality&lt;&gt;.Instance
         /// </summary>
         /// <typeparam name="T">The type of the objects to compare.</typeparam>
         public static bool AreEqual<T>(T a, T b) => FieldwiseEquality<T>.Instance(a, b);
     }
-    public static class FieldwiseEquality<T> {
+
+    /// <summary>
+    ///     Implements field-wise equality via reflection-based code generation.
+    /// </summary>
+    public static class FieldwiseEquality<T>
+    {
         /// <summary>
-        /// Checks whether two objects of the same type are field-wise equal.  Type resolution is specified 
-        /// statically, which allows fast code (similar to hand-rolled performance).
-        /// However, warning: if the objects are of compile-time type BaseType, but at runtime turn out
-        /// to be SubClass, then only the fields of BaseType will be checked.
+        ///     Checks whether two objects of the same type are field-wise equal.  Type resolution is specified
+        ///     statically, which allows fast code (similar to hand-rolled performance).
+        ///     However, warning: if the objects are of compile-time type BaseType, but at runtime turn out
+        ///     to be SubClass, then only the fields of BaseType will be checked.
         /// </summary>
         public static readonly Func<T, T, bool> Instance = CreateLambda().Compile();
 
-        internal static Expression<Func<T, T, bool>> CreateLambda() {
+        static Expression<Func<T, T, bool>> CreateLambda()
+        {
             //Get all fields including inherited fields
             var type = typeof(T);
             var fields = ReflectionHelper.GetAllFields(type);
@@ -40,54 +48,57 @@ namespace ValueUtils {
                 var aFieldExpr = Expression.Field(aExpr, fieldInfo);
                 var bFieldExpr = Expression.Field(bExpr, fieldInfo);
                 var bestEqualityApproach =
-                    EqualityByOperatorOrNull(aFieldExpr, bFieldExpr, fieldInfo)
-                    ?? InstanceEqualsOrNull(aFieldExpr, bFieldExpr, fieldInfo)
+                        EqualityByOperatorOrNull(aFieldExpr, bFieldExpr, fieldInfo)
+                        ?? InstanceEqualsOrNull(aFieldExpr, bFieldExpr, fieldInfo)
                     ;
 
                 eqExpr = Expression.AndAlso(eqExpr, bestEqualityApproach);
             }
+
             return Expression.Lambda<Func<T, T, bool>>(eqExpr, aExpr, bExpr);
         }
 
-        static bool HasEqualityOperator(TypeInfo type) {
-            return type.IsPrimitive
-                || type.IsEnum
-                || type.GetMethod("op_Equality", BindingFlags.Public | BindingFlags.Static) != null
-                || type.IsValueType && type.IsGenericType
-                        && type.GetGenericTypeDefinition() == typeof(Nullable<>)
-                        && HasEqualityOperator(type.GetGenericArguments()[0].GetTypeInfo());
-            //nullables are tricky: they are equatable by operator when their underlying type is.
-        }
-        static Expression EqualityByOperatorOrNull(Expression aFieldExpr, Expression bFieldExpr, FieldInfo fieldInfo) {
-            return HasEqualityOperator(fieldInfo.FieldType.GetTypeInfo())
+        static bool HasEqualityOperator(TypeInfo type) =>
+            type.IsPrimitive
+            || type.IsEnum
+            || type.GetMethod("op_Equality", BindingFlags.Public | BindingFlags.Static) != null
+            || WhenNullableValueTypeGetNonNullableType(type) is Type nonNullableType
+            && HasEqualityOperator(nonNullableType.GetTypeInfo());
+
+        static Type WhenNullableValueTypeGetNonNullableType(TypeInfo type)
+            => type.IsValueType && type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>)
+                ? type.GetGenericArguments()[0]
+                : null;
+
+        static Expression EqualityByOperatorOrNull(Expression aFieldExpr, Expression bFieldExpr, FieldInfo fieldInfo) =>
+            HasEqualityOperator(fieldInfo.FieldType.GetTypeInfo())
                 ? Expression.Equal(aFieldExpr, bFieldExpr)
                 : null;
-        }
 
-        static Expression InstanceEqualsOrNull(Expression aFieldExpr, Expression bFieldExpr, FieldInfo fieldInfo) {
+        static Expression InstanceEqualsOrNull(Expression aFieldExpr, Expression bFieldExpr, FieldInfo fieldInfo)
+        {
             var fieldType = fieldInfo.FieldType;
             var equalsMethod = fieldType.GetTypeInfo().GetMethod(
                 "Equals", new[] { fieldType });
 
             var fieldsEqualExpr = equalsMethod == null || equalsMethod.GetParameters()[0].ParameterType != fieldType
-                ? Expression.Call(((Func<object, object, bool>)Equals).GetMethodInfo(),
-                    Expression.Convert(aFieldExpr, typeof(object)),
-                    Expression.Convert(bFieldExpr, typeof(object))
-                )
-// ReSharper disable once PossiblyMistakenUseOfParamsMethod
-                : Expression.Call(aFieldExpr, equalsMethod, bFieldExpr)
+                    ? Expression.Call(((Func<object, object, bool>) Equals).GetMethodInfo(),
+                        Expression.Convert(aFieldExpr, typeof(object)),
+                        Expression.Convert(bFieldExpr, typeof(object))
+                    )
+                    : Expression.Call(aFieldExpr, equalsMethod, bFieldExpr)
                 ;
             // TODO: optimization possibility: if an object needs object.Equals, and it has *not* overridden
             // which is quite likely if you need it in the first place, then I just just use a reference equality for
             // reference types and a by-field equality for structs.
 
             var nullSafeFieldsEqualExpr = fieldInfo.FieldType.GetTypeInfo().IsValueType || equalsMethod == null
-                ? (Expression)fieldsEqualExpr
+                ? (Expression) fieldsEqualExpr
                 : Expression.Condition(
-                  Expression.Equal(Expression.Default(fieldInfo.FieldType), aFieldExpr),
-                     Expression.Equal(Expression.Default(fieldInfo.FieldType), aFieldExpr),
-                     fieldsEqualExpr
-               );
+                    Expression.Equal(Expression.Default(fieldInfo.FieldType), aFieldExpr),
+                    Expression.Equal(Expression.Default(fieldInfo.FieldType), aFieldExpr),
+                    fieldsEqualExpr
+                );
             return nullSafeFieldsEqualExpr;
         }
     }
