cc17ce16056b5174aaf62eb60a30a6df2661de08
Eamon Nerbonne <eamon@nerbonne.org>
2019-05-28 17:40:53 +0200

    Try to exclude benchmark from coverage, because pointless?

--- ValueUtilsBenchmark/ValueUtilsBenchmarkProgram.cs
+++ ValueUtilsBenchmark/ValueUtilsBenchmarkProgram.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 using ExpressionToCodeLib;
 using MoreLinq;
+
+[assembly: ExcludeFromCodeCoverage]
 
 namespace ValueUtilsBenchmark
 {
